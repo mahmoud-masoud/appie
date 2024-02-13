0c4430b52ab0b12c3d3569f75cbe94c4ef2d168b
mahmoud eldenshaly <mahmoudmasoud0000@gmail.com>
2024-02-13 22:58:01 +0200

    adding animations.css file and updating intersctin observer.js file

--- intersectionObserver.js
+++ intersectionObserver.js
@@ -1,59 +1,82 @@
-const statisticsCards = document.querySelectorAll('.card');
-const statisticsNumbers = document.querySelectorAll('.stc-num');
 const heroSection = document.querySelector('.hero-section');
 const header = document.querySelector('.header');
+const statisticsCards = document.querySelectorAll('.card');
+const statisticsNumbers = document.querySelectorAll('.stc-num');
+const sponsorsBoxes = document.querySelectorAll('.sponsor-box');
+const featuresImg = document.querySelector('.f-img-box');
+const heroImg = document.querySelector('.hero-img-box');
+const heroContent = document.querySelector('.hero-content');
 
-const heroSectionObserver = new IntersectionObserver(
-  (entries) => {
-    const entry = entries[0];
-    if (!entry.isIntersecting) {
-      header.classList.add('sticky');
-    } else {
-      header.classList.remove('sticky');
-    }
-  },
-  {
-    root: null,
-    threshold: 0,
+const heroHeading = heroContent.querySelector('h1');
+const heroText = heroContent.querySelector('p');
+const heroForm = heroContent.querySelector('form');
+const heroFeedbacks = heroContent.querySelector('.feedbacks');
+const heroElements = [heroHeading, heroText, heroForm, heroFeedbacks];
+
+const sectionHowCards = document.querySelectorAll('.section-how-card');
+
+const aboutImg = document.querySelector('.about-img-box');
+const aboutText = document.querySelector('.about-text');
+const aboutRow = document.querySelector('.about-row');
+
+const features = document.querySelectorAll('.feature');
+const sweetPlan = document.querySelector('.sweet');
+
+const navOptions = {
+  root: null,
+  threshold: 0,
+};
+
+const heroSectionObserver = new IntersectionObserver((entries) => {
+  const entry = entries[0];
+  if (!entry.isIntersecting) {
+    header.classList.add('sticky');
+  } else {
+    header.classList.remove('sticky');
   }
-);
+}, navOptions);
 
 heroSectionObserver.observe(heroSection);
 
-const cardsObserver = new IntersectionObserver((entries, observer) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('animation');
-      entry.target.style.animationDelay = entry.target.dataset.delay;
-      observer.disconnect();
-    }
-  });
-});
+const defaultObserverOptions = {
+  threshold: 0.5,
+  rootMargin: '50px 0px',
+};
 
-statisticsCards.forEach((card) => {
-  cardsObserver.observe(card);
-});
-
-const numbersObservers = new IntersectionObserver((entries, observer) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      const number = +entry.target.textContent;
-      let counter = 0;
-      const interval = setInterval(() => {
-        if (number > 200) {
-          entry.target.textContent = counter += 5;
-        } else {
-          entry.target.textContent = counter++;
+const applyAnimation = (items, animationClass, delay = 0) => {
+  const itemsObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry, index) => {
+      if (entry.isIntersecting) {
+        if (delay) {
+          entry.target.style.animationDelay = index * delay + 's';
         }
-        if (counter > number) {
-          clearInterval(interval);
-          observer.disconnect();
-        }
-      }, 10);
-    }
-  });
-});
 
-statisticsNumbers.forEach((num) => {
-  numbersObservers.observe(num);
-});
+        entry.target.classList.add(animationClass);
+
+        itemsObserver.unobserve(entry.target);
+      }
+    });
+  }, defaultObserverOptions);
+
+  if (items.length) {
+    items.forEach((item) => {
+      itemsObserver.observe(item);
+    });
+  } else {
+    itemsObserver.observe(items);
+  }
+};
+console.log(aboutRow.children);
+
+applyAnimation(heroImg, 'slide-in');
+applyAnimation(heroElements, 'fade-in', 0.2);
+applyAnimation(sectionHowCards, 'fade-in', 0.2);
+applyAnimation(aboutImg, 'fade-in');
+applyAnimation(aboutText, 'fade-in');
+applyAnimation([...aboutRow.children], 'about-row-animation', 0.2);
+
+applyAnimation(statisticsCards, 'card-animation', 0.3);
+applyAnimation(sponsorsBoxes, 'sponsor-box-animations', 0.15);
+applyAnimation(featuresImg, 'fade-in');
+applyAnimation(features, 'animated-feature', 0.3);
+applyAnimation(sweetPlan, 'sweet-plan-animations');
